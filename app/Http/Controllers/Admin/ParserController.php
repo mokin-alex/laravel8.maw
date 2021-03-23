ffb5cd06a0fbaaeb5f5e1b491469fa999a5d1891
Alex Mokin <mr.aleksay@gmail.com>
2021-03-23 11:29:19 +0500

    News Parser is ready

--- app/Http/Controllers/Admin/ParserController.php
+++ app/Http/Controllers/Admin/ParserController.php
@@ -3,34 +3,25 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use Orchestra\Parser\Xml\Facade as XmlParser;
+use App\Services\XMLParserServices;
 
 class ParserController extends Controller
 {
-    //
-    public function index() {
-        $xml = XmlParser::load('https://lenta.ru/rss');
+    public function index(XMLParserServices $parserServices)
+    {
+        $rssLinks = [
+            'https://lenta.ru/rss/news',
+            'https://news.yandex.ru/auto.rss',
 
-        dump($xml);
+        ];
 
-        $data = $xml->parse([
-            'title' => ['uses' => 'channel.title'],
-            'link' => ['uses' => 'channel.link'],
-            'description' => ['uses' => 'channel.description'],
-            'image' => ['uses' => 'channel.image.url'],
-            'news' => ['uses' => 'channel.item[title,link,guid,description,pubDate,enclosure::url]']
-        ]);
-
-        dd($data);
-
-        foreach ($data['news'] as $news) {
-            // Получить категорию (с id)
-            // при необходимости добавить в БД
-            // ПОлучить новость
-            // при необходимости добавить с id Категории
-            //HELP News::query()->firstOrCreate([])
+        foreach ($rssLinks as $rssLink) {
+            $parserServices->saveNews($rssLink);
+            //NewsParsing::dispatch($rssLink);
         }
+
+        return redirect()->route('admin.news.index');
+
     }
 
 }
